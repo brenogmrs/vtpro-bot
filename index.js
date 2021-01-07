227882d08b180d048f8d449e786cd6b54a93b3e1
Breno Guimaraes <brenoaraujo@outlook.com.br>
2021-01-06 22:51:02 -0300

    feat: final commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const input = require('readline-sync');
 
 async function fn () {
-    const browser = await puppeteer.launch({ headless:false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     
     await page.goto(process.env.VT_PRO_MAIN_URL);
@@ -15,6 +15,7 @@ async function fn () {
 
     // vai para o lançamento de horas
     await page.goto(process.env.VT_PRO_TIMESHEET_URL);
+    await page.waitForTimeout(2000);
 
     const oiToRegister = input.question('OI: ');
 
@@ -23,18 +24,42 @@ async function fn () {
 
     await page.evaluate((oiToRegister) => {
 
-        const example = document.querySelector('#ctl00_cphPrincipal_ddlAlocacaoRecurso');
-        const example_options = example.querySelectorAll('option');
-        const selected_option = [...example_options].find(option => option.text.slice(0, 7) === oiToRegister);
-      
-        selected_option.selected = true;
+        const oiSelector = document.querySelector('#ctl00_cphPrincipal_ddlAlocacaoRecurso');
+        const oiSelectorOptions = oiSelector.querySelectorAll('option');
+        setTimeout('__doPostBack(\'ctl00$cphPrincipal$ddlAlocacaoRecurso\',\'\')', 0)
+
+        const selectedOi = [...oiSelectorOptions].find(option => option.text.slice(0, 7) === oiToRegister);
+        const dayDescription = selectedOi.text.split('-')
+        document.querySelector('#ctl00_cphPrincipal_txtObs').value = dayDescription[1];
+
+        document.querySelector('#ctl00_cphPrincipal_txtHoraInicio1').value = "08:00";
+        document.querySelector('#ctl00_cphPrincipal_txtHoraFim1').value = "13:00";
+        document.querySelector('#ctl00_cphPrincipal_txtHoraInicio2').value = "14:00";
+        document.querySelector('#ctl00_cphPrincipal_txtHoraFim2').value = "17:00";
+
+        selectedOi.selected = true;
+
     }, oiToRegister);
 
-    
-    //await browser.close();
+    await page.waitForTimeout(2000);
+
+    await page.click('#ctl00_cphPrincipal_ddlDia');
+
+    await page.evaluate(() => {
+        const date = new Date();
+        const day = date.getDate()
+
+        const daySelector = document.querySelector('#ctl00_cphPrincipal_ddlDia');
+        const daySelectorOptions = daySelector.querySelectorAll('option');
+        const selectedDay = [...daySelectorOptions].find(option => option.text == day);
+
+        selectedDay.selected = true;
+
+    });
+
+    await page.click('#ctl00_cphPrincipal_btnSalvar');
+
+    await browser.close();
 }
-
-
-
 
 fn();
